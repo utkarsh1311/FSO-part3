0c9a2c10fbe3d34690853249c3b16c9a9c3a7df9
Utkarsh <itsutkarsh123@gmail.com>
2022-06-04 20:10:54 +0530

    add solution for exercise 3.2

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ app.get("/api/persons", (request, response) => {
 	response.json(persons);
 });
 
+app.get("/info", (request, response) => {
+	response.send(
+		`Phonebook has info for ${persons.length} people <br/> ${new Date()}`
+	);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
 	console.log(`Server running at port ${PORT}`);
