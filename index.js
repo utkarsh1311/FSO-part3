42f3be83467b88593959d5525e1743ca0bb286f9
Utkarsh <itsutkarsh123@gmail.com>
2022-08-10 22:36:25 +0530

    add solution for 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const cors = require("cors");
+
+const Person = require('./models/person');
 
 app.use(express.json());
 app.use(express.static("build"));
@@ -11,37 +15,14 @@ app.use(
 	morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-	{
-		id: 1,
-		name: "Arto Hellas",
-		number: "040-123456",
-	},
-	{
-		id: 2,
-		name: "Ada Lovelace",
-		number: "39-44-5323523",
-	},
-	{
-		id: 3,
-		name: "Dan Abramov",
-		number: "12-43-234345",
-	},
-	{
-		id: 4,
-		name: "Mary Poppendieck",
-		number: "39-23-6423122",
-	},
-];
-
-const generateID = () => Math.floor(Math.random() * 100);
-
 app.get("/", (request, response) => {
 	response.send("Hello world");
 });
 
 app.get("/api/persons", (request, response) => {
-	response.json(persons);
+	Person.find({}).then(persons => {
+        response.json(persons);
+    })
 });
 
 app.get("/info", (request, response) => {
@@ -69,12 +50,6 @@ app.delete("/api/persons/:id", (request, response) => {
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
 
-	if (persons.some((p) => p.name === body.name)) {
-		return response.status(400).json({
-			error: "name must be unique",
-		});
-	}
-
 	if (!body.name && !body.number) {
 		return response.status(400).json({
 			error: "name and number is not specified in the request body",
@@ -91,14 +66,12 @@ app.post("/api/persons", (request, response) => {
 		});
 	}
 
-	const newPerson = {
-		id: generateID(),
-		name: body.name,
-		number: body.number,
-	};
+	const newPerson = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-	persons = persons.concat(newPerson);
-	response.json(newPerson);
+	newPerson.save().then(savedPerson => response.json(savedPerson));
 });
 
 const PORT = process.env.PORT || 3001;
---
+++ models/person.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+    .connect(url)
+    .then(result => {
+        console.log("Connected to MongoDB");
+    })
+    .catch(error => {
+        console.err("Error connecting to the MongoDB: ", error.message);
+    });
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+});
+
+personSchema.set("toJSON", {
+    transform: (document, returnedPerson) => {
+        returnedPerson.id = returnedPerson._id.toString()
+        delete returnedPerson._id
+        delete returnedPerson.__v
+    },
+})
+
+module.exports = mongoose.model("Person", personSchema);
